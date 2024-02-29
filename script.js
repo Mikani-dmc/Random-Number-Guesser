bd534151522387ca3e628a004ea6aaebfde88a88
Daniela Marques Correia <danedcmc@gmail.com>
2024-02-29 17:01:05 -0300

    made it better

--- script.js
+++ script.js
@@ -3,42 +3,47 @@ const send = document.getElementById("send");
 const audioSeCorreto  = document.getElementById("audioSeCorreto");
 const result = document.getElementById("result");
 const tentativasFeitas = document.getElementById("tentativas");
+const audioSeErrado= document.getElementById("audioSeErrado");
+const container = document.getElementById("container");
 
-let randomNumber = Math.floor(Math.random()*100 + 1)
+let randomNumber = Math.floor(Math.random()*100 + 1);
 let isCorrect = false;
 let tentativas = 0; 
+audioSeCorreto.volume = 0.1;
+audioSeErrado.volume = 0.1;
 
 console.log(typeof(randomNumber), randomNumber);
+
+function seErrado() {
+    tentativas++;
+    audioSeErrado.play();
+    container.classList.add("errado");
+}
 
 function enviarResposta() {    
     const userGuess = parseInt(tentativaDoUsuario.value);
     if (userGuess != Number(userGuess) || userGuess>100 || userGuess < 1){
         result.innerText = "Entrada inválida";
-        tentativaDoUsuario.innerText = " ";
-        tentativas++;
-        tentativasFeitas.innerText = "Tentativas Feitas " + tentativas;
-        tentativaDoUsuario.className = "errado"
-  
+        tentativaDoUsuario.innerText = " ";  
+        seErrado();
     }
     else if (userGuess > randomNumber){
         result.innerText = "ERRADO! Tente um numero mais baixo"
         tentativaDoUsuario.innerText = " "
-        tentativas++;
-        tentativasFeitas.innerText = "Tentativas Feitas " +  tentativas;
- 
+        seErrado(); 
     }
     else if (userGuess  < randomNumber){
         result.innerText= "ERRADO! Tente um numero mais alto"
         tentativaDoUsuario.innerText = " "
-        tentativas++;
-        tentativasFeitas.innerText = "Tentativas Feitas " + tentativas;
-      
+        seErrado();      
     }
     else{
         audioSeCorreto.play();
         result.innerText= `Parabéns! O número correto era ${randomNumber}`
+        container.classList.add("coreto");
         isCorrect = true;            
         tentativasFeitas.innerText = "Tentativas Feitas " + tentativas;
+        send.classList.add("hidden");
     }
 
 }
